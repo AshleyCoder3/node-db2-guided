4d0ed92df2d9087da3b853f2688ea101593e72df
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 19:50:09 +0200

    wip

--- data/migrations/20210907173910_make-fruits-table.js
+++ data/migrations/20210907173910_make-fruits-table.js
@@ -2,7 +2,7 @@ exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table => {
     table.increments() // this create a primary key called id of auto inc integers
     table.text('name', 128).unique().notNullable()
-    table.decimal('avgFruitOz').notNullable()
+    table.decimal('avgWeightOz').notNullable()
     table.boolean('delicious')
   })
 }
