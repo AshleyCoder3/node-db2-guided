5185feb90eee9a2fe548a375ce520e722b0b915c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 19:55:29 +0200

    wip

--- data/migrations/20210907175421_add-color-to-fruits.js
+++ data/migrations/20210907175421_add-color-to-fruits.js
@@ -1,9 +1,6 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable('fruits', table => {
-    table.increments() // this create a primary key called id of auto inc integers
-    table.text('name', 128).unique().notNullable()
-    table.decimal('avgWeightOz').notNullable()
-    table.boolean('delicious')
+  await knex.schema.table('fruits', table => {
+    table.string('color', 120)
   })
 }
 
