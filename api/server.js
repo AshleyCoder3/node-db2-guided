6a2604552c3e31474bebeebd27c21156dcafbc53
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 18:32:14 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ server.use(express.json());
 
 server.use('/api/fruits', fruitsRouter);
 
-server.use((err, req, res, next) => { // e
+server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
