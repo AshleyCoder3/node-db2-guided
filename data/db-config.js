6c3d7f6a6ec7cf8fc7d28473ef15faf9716dcf51
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 18:24:37 +0200

    wip

--- data/db-config.js
+++ data/db-config.js
@@ -3,3 +3,4 @@ const knex = require('knex')
 // these are the possible configs
 const configs = require('../knexfile')
 // what environment are we on?
+const env = process.env.NODE_ENV || 'development'
