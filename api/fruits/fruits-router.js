d486688426d7b99ed40977ebbeaa5ca976856c61
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 18:55:56 +0200

    wip

--- api/fruits/fruits-router.js
+++ api/fruits/fruits-router.js
@@ -22,20 +22,7 @@ router.get('/:id', async (req, res, next) => {
 });
 
 router.post('/', (req, res) => {
-  const fruitData = req.body;
-  db('fruits').insert(fruitData)
-    .then(ids => {
-      return db('fruits').where({ id: ids[0] })
-
-    })
-
-    .then(newFruitEntry => {
-      res.status(201).json(newFruitEntry);
-    })
-    .catch(err => {
-      console.log('POST error', err);
-      res.status(500).json({ message: "Failed to store data" });
-    });
+    
 });
 
 module.exports = router;
