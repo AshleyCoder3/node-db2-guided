ee422e6b85699fbaa7ab230b085037a6a2941be3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-07 20:08:43 +0200

    wip

--- data/seeds/add-fruits.js
+++ data/seeds/add-fruits.js
@@ -3,8 +3,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries AND reset the primary keys
   await knex('fruits').truncate()
   await knex('fruits').insert([
-    { name: 'orange' },
-    { name: 'orange' },
-    { name: 'orange' }
+    { avgWeightOz: 23, name: 'orange' },
+    { avgWeightOz: 10, name: 'pear',  },
+    { avgWeightOz: 2.6, name: 'apple' }
   ]);
 };
